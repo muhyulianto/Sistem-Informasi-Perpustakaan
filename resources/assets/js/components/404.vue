de46eb87a068f2118f46d456d2cc0f9c0c827255
muhyulianto <myulianto72@gmail.com>
2020-04-06 12:45:33 +0700

    membuat halaman not found

---
+++ resources/assets/js/components/404.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="container">
+    <div class="row justify-content-center vh-100">
+      <div class="col-lg-4 align-self-center">
+        <p class="text-center">
+          Opss something wrong . . .
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
